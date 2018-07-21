b788df8f3ad0d4607fa59e6eeb7ede719c65f90f
Titus Fortner <titusfortner@gmail.com>
2018-07-21 11:05:28 -0700

    fix auto-commit value

--- deploy.sh
+++ deploy.sh
@@ -45,7 +45,7 @@ fi
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add -A .
-git commit -m "Deploy Generated Content from Travis ${#$TRAVIS_BUILD_NUMBER}: ${SHA}"
+git commit -m "Deploy Generated Content from Travis ${TRAVIS_BUILD_NUMBER}: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
